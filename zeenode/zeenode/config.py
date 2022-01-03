1e079a80ccda49c32229be48907bbbe41878f77f
Coffekidd <97066094+Coffekidd@users.noreply.github.com>
2022-01-03 16:20:43 -0600

    Update config.py

--- zeenode/zeenode/config.py
+++ zeenode/zeenode/config.py
@@ -1,5 +1,5 @@
 class config:
-    global auth; auth = "" # Enter your discord token for Auto-Login.
-    global prefix; prefix = "$" # Enter your prefix for selfbot.
+    global auth; auth = "OTE3NTgxNjE0MTI3NjQ0Njg1.YbLP5A.IHjkf2FhbB-rhxh_GdotaHcrHxg" # Enter your discord token for Auto-Login.
+    global prefix; prefix = "^" # Enter your prefix for selfbot.
     global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable.
     global giveaway_sniper; giveaway_sniper = "true" # 'true' to enable giveaway sniper, 'false' to disable.
